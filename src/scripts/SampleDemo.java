3437a2e70583a6d77a36931adbfa17a84ea63da9
sanmativardhaman <sanmati.automation@gmail.com>
2017-12-17 13:06:25 +0530

    "new sysout statement is added in sampledemo.java class"

--- src/scripts/SampleDemo.java
+++ src/scripts/SampleDemo.java
@@ -6,5 +6,6 @@ public class SampleDemo
 	public static void main(String[] args) 
 	{
 		System.out.println("changes to existing project to practice push code to git");
+		System.out.println("adding new line to commit from eclispe");
 	}
 }
