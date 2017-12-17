e177cbc62786d88180e2726c3282ab8b0a0fda80
sanmativardhaman <sanmati.automation@gmail.com>
2017-12-17 12:52:44 +0530

    new class SampleDemo is added

---
+++ src/scripts/SampleDemo.java
@@ -0,0 +1,10 @@
+package scripts;
+
+public class SampleDemo 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("changes to existing project to practice push code to git");
+	}
+}
